0664366c572e942d0548959def0626c22cda8fd6
Schifelbain <daniel.schifelbain@capgemini.com>
2024-01-08 13:08:47 -0300

    Created user-display component

---
+++ src/components/user-display/user-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-display.component.html',
+  styleUrl: './user-display.component.css'
+})
+export class UserDisplayComponent {
+
+}
