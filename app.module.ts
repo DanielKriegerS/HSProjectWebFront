0e9edc2913d246f5475a6a9103144e5d3851e004
Schifelbain <daniel.schifelbain@capgemini.com>
2024-01-11 18:22:33 -0300

    Added navbar

--- app.module.ts
+++ app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { UserDisplayComponent } from './src/components/user-display/user-display.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './src/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     MainPageComponent,
     PostsComponent,
     CommentsComponent,
+    NavbarComponent,
     UserDisplayComponent,
     ReactionsComponent
   ],
