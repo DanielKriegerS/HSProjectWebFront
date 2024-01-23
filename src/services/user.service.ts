fbe29074fa958dfe1a4055271bd0bf7e73e3973d
dschifel <danielschifelbain@gmail.com>
2024-01-23 16:49:15 -0300

    Adjusted user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,18 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { User } from '../models/user'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080'
+  private apiUrl = 'http://localhost:8080';
+  private updateUserSubject = new Subject<void>();
 
   constructor(private http: HttpClient) { }
 
   updateUser(userId: string, updatedUserData: any): Observable<User> {
-    return this.http.put<User>(`${this.apiUrl}/users/${userId}`, updatedUserData);
+    const updateUserObservable = this.http.put<User>(`${this.apiUrl}/users/${userId}`, updatedUserData);
+
+    updateUserObservable.subscribe(() => {
+      this.updateUserSubject.next();
+    });
+
+    return updateUserObservable;
+  }
+
+  getUpdateUserObservable() {
+    return this.updateUserSubject.asObservable();
   }
 
   getUserById(userId: string): Observable<User> {
