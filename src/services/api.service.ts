a05087fbfcea3c7edb25ac7928c63be176ddaa34
dschifel <danielschifelbain@gmail.com>
2024-01-29 15:46:57 -0300

    Splited posts services of api service into post service

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Post } from '../models/post';
 import { Comment } from '../models/comment';
 
 @Injectable({
@@ -18,15 +17,5 @@ export class ApiService {
 
   getCommentsByPostId(postId: string): Observable<Comment[]> {
     return this.http.get<Comment[]>(`${this.apiUrl}/comments/commentsByPostId/${postId}`);
-  }
-
- 
-  getPostsByUserId(userId: string): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.apiUrl}/posts/user/${userId}`);
-  }
-
-  createPost(newPost: Post): Observable<Post> {
-    console.log(newPost)
-    return this.http.post<Post>(`${this.apiUrl}/posts`, newPost);
   }
 }
--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+  getPostsByUserId(userId: string): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.apiUrl}/posts/user/${userId}`);
+  }
+
+  createPost(newPost: Post): Observable<Post> {
+    console.log(newPost)
+    return this.http.post<Post>(`${this.apiUrl}/posts`, newPost);
+  }
 }
