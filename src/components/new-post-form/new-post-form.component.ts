fb9afd1cef3327f5fc6c865c4f00d513d70b5901
dschifel <danielschifelbain@gmail.com>
2024-01-29 15:50:55 -0300

    Created new-post-form component

---
+++ src/components/new-post-form/new-post-form.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../../models/post';
+
+@Component({
+  selector: 'app-new-post-form',
+  templateUrl: './new-post-form.component.html',
+  styleUrl: './new-post-form.component.css'
+})
+
+export class NewPostFormComponent {
+  @Input() userId!:string;
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSubmit = new EventEmitter<Post>();
+
+
+  newPostForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.newPostForm = this.fb.group({
+      header: ['', Validators.required],
+      desc: ['', Validators.required],
+      body: ['', Validators.required],
+    });
+  }
+  submitPost(): void {
+    if (this.newPostForm.valid) {
+       const newPost: Post = {
+         userId: this.userId, 
+         header: this.newPostForm.get('header')?.value,
+         desc: this.newPostForm.get('desc')?.value,
+         body: this.newPostForm.get('body')?.value
+       };
+       console.log(newPost);
+       this.onSubmit.emit(newPost);
+       this.newPostForm.reset();
+     }
+   }
+ 
+   cancel(): void {
+     this.onCancel.emit();
+     this.newPostForm.reset();
+   }
+}
